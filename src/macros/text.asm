e45f1887e3b3814a44df287603d8d74fb094462b
ElectroDeoxys <ElectroDeoxys@gmail.com>
2023-09-02 12:23:12 +0100

    Fix RGBDS text macro warnings

--- src/macros/text.asm
+++ src/macros/text.asm
@@ -4,18 +4,28 @@ DEF done EQUS "db TX_END"
 
 DEF half2full EQUS "db TX_HALF2FULL"
 
-MACRO katakana
-	db TX_KATAKANA
-	REPT _NARG
+MACRO kanachar
+	IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
+		db \1
+	ELIF STRLEN(\1) > 4
 		IF STRCMP(STRSUB(\1, 1, 4), "FW0_") == 0
-			db \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
 			db \1
 		ELSE
 			FOR i, STRLEN(\1)
 				db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
 			ENDR
 		ENDC
+	ELSE
+		FOR i, STRLEN(\1)
+			db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
+		ENDR
+	ENDC
+ENDM
+
+MACRO katakana
+	db TX_KATAKANA
+	REPT _NARG
+		kanachar \1
 		SHIFT
 	ENDR
 ENDM
@@ -23,25 +33,23 @@ ENDM
 MACRO hiragana
 	db TX_HIRAGANA
 	REPT _NARG
-		IF STRCMP(STRSUB(\1, 1, 4), "FW0_") == 0
-			db \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
-			db \1
-		ELSE
-			FOR i, STRLEN(\1)
-				db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
-			ENDR
-		ENDC
+		kanachar \1
 		SHIFT
 	ENDR
 ENDM
 
 MACRO textfw0
 	REPT _NARG
-		IF STRCMP(STRSUB(\1, 1, 4), "FW0_") == 0
+		IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
 			db \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
-			db \1
+		ELIF STRLEN(\1) > 4
+			IF STRCMP(STRSUB(\1, 1, 4), "FW0_") == 0
+				db \1
+			ELSE
+				FOR i, STRLEN(\1)
+					db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
+				ENDR
+			ENDC
 		ELSE
 			FOR i, STRLEN(\1)
 				db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
@@ -53,10 +61,20 @@ ENDM
 
 MACRO textfw1
 	REPT _NARG
-		IF STRCMP(STRSUB(\1, 1, 4), "FW1_") == 0
-			db TX_FULLWIDTH1, \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
+		IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
 			db \1
+		ELIF STRLEN(\1) > 4
+			IF STRCMP(STRSUB(\1, 1, 4), "FW1_") == 0
+				db TX_FULLWIDTH1, \1
+			ELSE
+				FOR i, STRLEN(\1)
+					IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
+						db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
+					ELSE
+						db TX_FULLWIDTH1, STRCAT("FW1_", STRSUB(\1, i + 1, 1))
+					ENDC
+				ENDR
+			ENDC
 		ELSE
 			FOR i, STRLEN(\1)
 				IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
@@ -72,10 +90,20 @@ ENDM
 
 MACRO textfw2
 	REPT _NARG
-		IF STRCMP(STRSUB(\1, 1, 4), "FW2_") == 0
-			db TX_FULLWIDTH2, \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
+		IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
 			db \1
+		ELIF STRLEN(\1) > 4
+			IF STRCMP(STRSUB(\1, 1, 4), "FW2_") == 0
+				db TX_FULLWIDTH2, \1
+			ELSE
+				FOR i, STRLEN(\1)
+					IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
+						db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
+					ELSE
+						db TX_FULLWIDTH2, STRCAT("FW2_", STRSUB(\1, i + 1, 1))
+					ENDC
+				ENDR
+			ENDC
 		ELSE
 			FOR i, STRLEN(\1)
 				IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
@@ -91,10 +119,20 @@ ENDM
 
 MACRO textfw3
 	REPT _NARG
-		IF STRCMP(STRSUB(\1, 1, 4), "FW3_") == 0
-			db TX_FULLWIDTH3, \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
+		IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
 			db \1
+		ELIF STRLEN(\1) > 4
+			IF STRCMP(STRSUB(\1, 1, 4), "FW3_") == 0
+				db TX_FULLWIDTH3, \1
+			ELSE
+				FOR i, STRLEN(\1)
+					IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
+						db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
+					ELSE
+						db TX_FULLWIDTH3, STRCAT("FW3_", STRSUB(\1, i + 1, 1))
+					ENDC
+				ENDR
+			ENDC
 		ELSE
 			FOR i, STRLEN(\1)
 				IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
@@ -110,10 +148,20 @@ ENDM
 
 MACRO textfw4
 	REPT _NARG
-		IF STRCMP(STRSUB(\1, 1, 4), "FW4_") == 0
-			db TX_FULLWIDTH4, \1
-		ELIF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
+		IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
 			db \1
+		ELIF STRLEN(\1) > 4
+			IF STRCMP(STRSUB(\1, 1, 4), "FW4_") == 0
+				db TX_FULLWIDTH4, \1
+			ELSE
+				FOR i, STRLEN(\1)
+					IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
+						db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
+					ELSE
+						db TX_FULLWIDTH4, STRCAT("FW4_", STRSUB(\1, i + 1, 1))
+					ENDC
+				ENDR
+			ENDC
 		ELSE
 			FOR i, STRLEN(\1)
 				IF STRCMP(STRSUB(\1, i + 1, 1), " ") == 0
